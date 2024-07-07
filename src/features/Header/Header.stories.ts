0bae600dc49eeb7f2b7eddb31447f5d12948e8da
i-yuya <yuyatuta@gmail.com>
2024-07-08 00:38:31 +0900

    storybookにdaisyuiを適用

--- src/features/Header/Header.stories.ts
+++ src/features/Header/Header.stories.ts
@@ -1,29 +1,28 @@
-import Header from "$/features/Header/Header.sveltemport type { NaviItem } from '$/types/NaviItem';
+import Header from "$/features/Header/Header.svelte";
+import type { NaviItem } from '$/types/NaviItem';
+import type { Meta, StoryObj } from '@storybook/svelte';
 
-export default {
-  title: 'Components/Header',
+const meta: Meta<Header> = {
+  title: "Components/Header",
   component: Header,
-  argTypes: {
-    title: { control: 'text' },
-    menu: {
-      control: {
-        type: 'object',
-      },
-    },
+  tags: ["autodocs"],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/7.0/svelte/configure/story-layout
+    layout: "fullscreen",
   },
 };
 
-const Template = ({ title, menu }: { title: string; menu: NaviItem[] }) => ({
-  Component: Header,
-  props: { title, menu },
-});
+export default meta;
 
-export const Default = Template.bind({});
-Default.args = {
-  title: 'My Website',
-  menu: [
-    { title: 'Home', path: '/' },
-    { title: 'About', path: '/about' },
-    { title: 'Contact', path: '/contact' },
-  ],
-};
+type Story = StoryObj<Header>;
+
+export const Default: Story = {
+  args: {
+    title: "Emotion Clicker",
+    menu: [
+      { title: "嬉しさを共有", path: "/" },
+      { title: "怒りを発散", path: "/" },
+      { title: "使い方", path: "/" },
+    ],
+  }
+}
